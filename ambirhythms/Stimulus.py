05888294c1083ea4d01dd2b60e189626990a3da3
Matt <matt.d.moore@outlook.com>
2022-03-04 21:07:09 +0100

    Converted Stimulus class to child of psychopy.Sound class

--- ambirhythms/Stimulus.py
+++ ambirhythms/Stimulus.py
@@ -1,36 +1,40 @@
+from psychopy import prefs; prefs.hardware['audioLib'] = ['PTB']
+from psychopy.sound import Sound
 from numpy import linspace, sin, pi
 
 
-class Stimulus:
-    def __init__(self, durations, ioi, attack=2, decay=48):
-        self.y = []
-        self.t = []
-        self.fs = 44.1e3
+class Stimulus(Sound):
+    def __init__(self, durations, ioi, fs=44.1e3, attack=2, decay=48, hz=440):
+        self.value = []
+        self.time = []
 
+        self.sampleRate = fs
         self.ioi = ioi
         self.attack = attack
         self.decay = decay
+        self.hz = hz
 
         self.generate_rhythm(durations)
+        super().__init__(self.value, sampleRate=fs)
 
     def generate_rhythm(self, durations):
         for d in durations:
             self.append_sine()
             self.append_silence(d * self.ioi - (self.attack + self.decay))
-        self.t = linspace(0, len(self.y) / self.fs, num=len(self.y)) * 1000
+        self.time = linspace(0, len(self.value) / self.sampleRate, num=len(self.value)) * 1e3
 
     def append_silence(self, ms):
         n = self.n_samples(ms)
         for _ in range(int(n)):
-            self.y.append(0.0)
+            self.value.append(0.0)
 
-    def append_sine(self, hz=440):
+    def append_sine(self):
         envelope = self.envelope(self.attack, self.decay)
         for i, x in enumerate(envelope):
-            self.y.append(x * sin(2 * pi * hz * (i / self.fs)))
+            self.value.append(x * sin(2 * pi * self.hz * (i / self.sampleRate)))
 
     def n_samples(self, ms):
-        return ms * (self.fs / 1000)
+        return ms * (self.sampleRate / 1e3)
 
     def envelope(self, attack, decay):
         attack_samples = int(self.n_samples(attack))
--- scripts/generate_audio.py
+++ scripts/generate_audio.py
@@ -1,8 +1,6 @@
-from psychopy import prefs; prefs.hardware['audioLib'] = ['PTB']
-from psychopy.sound import Sound
-
-from src.Rhythms import Rhythms
-from src.Stimulus import Stimulus
+from ambirhythms.Rhythms import Rhythms
+from ambirhythms.Stimulus import Stimulus
+from ambirhythms.DrumPad import DrumPad
 
 from numpy import nonzero
 import matplotlib.pyplot as plt
@@ -18,18 +16,23 @@ unambiguous = [135, 131, 175, 300, 200,
                11, 140, 24, 75, 136]
 
 idx = 57
+ioi = 200
 rhythm = rhythms[idx]
 rotations = nonzero(rhythm.onsets)[0]
 
+drum_pad = DrumPad('SPD')
 for r in rotations:
-    obj = Stimulus(rhythm.rotate(r).durations())
+    obj = Stimulus(rhythm.rotate(r).durations(), ioi)
 
-    sound = Sound(obj.y, sampleRate=obj.fs)
-    sound.play(loops=5)
+    obj.play(loops=6)
+    while obj.status == 1 and not drum_pad.beat_found:
+        beat, rotation = drum_pad.find_beat(ioi, verbose=True)
+    obj.stop()
+    drum_pad.reset()
 
     m = [i * obj.ioi for i in range(12)]
 
-    plt.plot(obj.t, obj.y)
-    plt.vlines(m, min(obj.y), max(obj.y), color='red')
+    plt.plot(obj.time, obj.value)
+    plt.vlines(m, min(obj.value), max(obj.value), color='red')
     plt.title('Rhythm {0}\nRotation {1}'.format(idx, r))
     plt.show()
