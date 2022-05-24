1aea9349043f5c9dcb39062598d9aa9b12343533
Matt <matt.d.moore@outlook.com>
2022-05-24 09:22:08 +0200

    Skip practice on resume

--- __main__.py
+++ __main__.py
@@ -2,30 +2,20 @@ import ambirhythms
 from os import chdir, listdir
 from scripts import pseudorandomiser, experiment, practice
 
-# TODO:
-#   Build visual components
-#       - Instructions, messages for breaks
-#       - Explain yellow shades feedback
-#   Practice trial
-#       - Example using 16-unit metre with clear beats
-#       - Experimenter check that participant is tapping hard enough
-#   Assemble full experiment (no visuals)
-#     x - Counterbalance blocked and randomised lists
-#     x - Inter- and intra-block breaks (both skippable but intra-block timed to 2min?)
-#     x - Jitter inter-trial interval between .5 and 1s
-#   Package for install
-#       - Check on clean Python environment in Windows first, then lab install
-#            - Runs on laptop, soundcard(?) issues on PC; check with soundcard on laptop
 
 if __name__ == '__main__':
     if '__main__.py' not in listdir():  # if called as project folder
         chdir('ambirhythms')
 
     participant_id, finish_state = ambirhythms.last_cached()
+    resume = any([state is not None for state in finish_state])
+
     blocks = pseudorandomiser.main(participant_id)
 
     drum_pad = ambirhythms.DrumPad('SPD')
-    window = ambirhythms.Screen((400, 400))
+    window = ambirhythms.Screen(size=(2000, 1125))
+    window.welcome_screen(resume)
 
-    practice.main(window, drum_pad, participant_id)
+    if not resume:
+        practice.main(window, drum_pad, participant_id)
     experiment.main(window, drum_pad, participant_id, finish_state, blocks)
